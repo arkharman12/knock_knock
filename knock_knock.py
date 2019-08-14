01ac9864a968feddc8b13d8c6556162fa93934cb
arkharman12 <53625021+arkharman12@users.noreply.github.com>
2019-08-13 20:07:44 -0400

    created knock_knock.py

---
+++ knock_knock.py
@@ -0,0 +1,9 @@
+print("WELCOME TO MY MAGICAL JOKE GENERATOR\n")
+
+name = input("Hello! What is your name: ")             #Asks the user for a input
+print("Here's a joke for you, {} \n".format(name))     #print a line to console window and put user's name in the placeholder
+whos_there = input("KNOCK, KNOCK: ")                   #stores user's input in a variable
+print("Harry.")                                        #print Harry to the window
+whos_luke = input()                                    #get user's input again
+print("Harry up, it's cold out here!")                 #print a line to the screen
+
